7e2edc8a46607a71ab4037dabf308546c4f25657
EC2 Default User <ec2-user@ip-172-31-17-112.us-east-2.compute.internal>
2018-06-06 06:53:36 +0000

    add column

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -4,7 +4,9 @@
 
 <h1>id = {{ $tasklist->id }} のタスクリスト詳細ページ</h1>
 
+    <p>ステータス: {{ $tasklist->status }}</p>
     <p>{{ $tasklist->content }}</p>
+    
 
     {!! link_to_route('tasklists.edit', 'このタスクリスト編集', ['id' => $tasklist->id]) !!}
 
