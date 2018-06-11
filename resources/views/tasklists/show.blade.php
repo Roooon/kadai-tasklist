c65b54a5b91e11d1d936c664bc0d2f7b5cae5cd8
EC2 Default User <ec2-user@ip-172-31-17-112.us-east-2.compute.internal>
2018-06-11 04:04:21 +0000

    Procfile

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -4,7 +4,7 @@
 
 <h1>id = {{ $tasklist->id }} のタスクリスト詳細ページ</h1>
  <div class="row">
-  <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+  <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
    <table class="table table-bordered">
         <tr>
             <th>id</th>
